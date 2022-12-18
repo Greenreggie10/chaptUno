9251ec46639b071be1d543f185b51a9b8bdfa852
Reginald Green <reggie.green10@yahoo.com>
2022-12-18 01:35:22 -0500

    "UPDATE"

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,13 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("RRRRR     G");
+        System.out.println("R   R   G   G");
+        System.out.println("R   R   G   G");
+        System.out.println("RRRR    G   ");
+        System.out.println("R   R   G  GGG");
+        System.out.println("R   R`  G   G");
+        System.out.println("R   R     G");
     }
 }
